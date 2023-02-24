0bcd1e2cedc1e0fb7899d7c60256b5b1bc0c76a1
heeyeon3050 <kho200a@naver.com>
2023-02-24 23:51:16 +0900

    update

---
+++ src/main/java/org/example/level0/test120820/Main.java
@@ -0,0 +1,13 @@
+package org.example.level0.test120820;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new org.example.level0.test120820.Solution().solution(40));
+    }
+}
+
+class Solution {
+    public int solution(int age) {
+        return 2023 - age;
+    }
+}
