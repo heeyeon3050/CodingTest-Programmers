88f6f6202fa0d1bac84a6f9c7623b42771bbe48f
heeyeon3050 <kho200a@naver.com>
2023-02-26 23:16:22 +0900

    update

---
+++ src/main/java/org/example/level0/test120831/Main.java
@@ -0,0 +1,17 @@
+package org.example.level0.test120831;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(10)); // -1
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for(int i=2; i<=n; i+=2){
+            sum += i;
+        }
+        return sum;
+    }
+}
