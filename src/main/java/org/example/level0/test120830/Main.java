5701e59bd1b9ea81f75d82d7808c8ec86caf40f7
heeyeon3050 <kho200a@naver.com>
2023-02-28 00:00:52 +0900

    update

--- src/main/java/org/example/level0/test120830/Main.java
+++ src/main/java/org/example/level0/test120830/Main.java
@@ -2,13 +2,12 @@ package org.example.level0.test120830;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(6, 4));
+        System.out.println(new Solution().solution(64, 6));
     }
 }
 class Solution {
     public int solution(int n, int k) {
         int sale1 = n/10;
-        int sale2 = k/10;
-        return (n*12000 + k*2000) - (2000*sale1 + 2000*sale2);
+        return (n*12000 + k*2000) - (2000*sale1);
     }
 }
