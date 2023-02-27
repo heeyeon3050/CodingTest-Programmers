609bb0cf9f6a3aa67052c3a70cb46cd10e421bcb
heeyeon3050 <kho200a@naver.com>
2023-02-27 23:56:45 +0900

    update

--- src/main/java/org/example/level0/test120831/Main.java
+++ src/main/java/org/example/level0/test120831/Main.java
@@ -2,7 +2,7 @@ package org.example.level0.test120831;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(10)); // -1
+        System.out.println(new Solution().solution(10));
     }
 }
 
